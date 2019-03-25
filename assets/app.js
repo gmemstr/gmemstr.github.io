acabf1815f625088547870387c13c063b9dce9c0
Gabriel Simmer <gsimmer@acromedia.com>
2019-03-24 21:50:27 -0700

    Dynamic fetching of repos from github, link static projects.

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,48 @@
 const carousel = document.getElementById("carousel");
-const projects = [
-    {
-        "name": "Pogo",
-        "description": "Self hosted podcast blah blah"
-    },
-    {
-        "name": "Database Janitor",
-        "description": "Customized MySQL database dumps"
-    }
-]
+let projects = [];
+window.onload = getProjects;
 
+/**
+ * Pick random project and place it in the carousel.
+ */
 function carouselProjectPicker() {
     let pointer = Math.round(Math.random() * 10 % projects.length) - 1;
     if (pointer < 0) pointer = 1;
-    console.log(pointer);
-    let project = projects[pointer];
 
-    carousel.innerHTML = "<h2>" + project.name + "<small>" + project.description + "</small></h2>";
+    // Create initial anchor element.
+    let project = projects[pointer];
+    let projectElement = document.createElement("a");
+    projectElement.href = project.html_url;
+    projectElement.target = "_blank";
+    projectElement.id = "carousel-project";
+
+    // Create project title element.
+    let projectHeader = document.createElement("h2");
+    let projectHeaderText = document.createTextNode(project.name);
+    projectHeader.appendChild(projectHeaderText);
+
+    // Create project description element.
+    let projectHeaderSmall = document.createElement("small");
+    let projectHeaderSmallText = document.createTextNode(project.description);
+    projectHeaderSmall.appendChild(projectHeaderSmallText);
+    projectHeader.appendChild(projectHeaderSmall);
+    projectElement.appendChild(projectHeader);
+
+    let oldProject = document.getElementById("carousel-project");
+    carousel.replaceChild(projectElement, oldProject);
 }
 
-window.onload = carouselProjectPicker();
-
-setInterval(carouselProjectPicker, 10000);
+/**
+ * Fetch projects from GitHub and trigger carousel event and interval.
+ */
+function getProjects() {
+    fetch("https://api.github.com/users/gmemstr/repos")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (apiResult) {
+        projects = apiResult;
+        carouselProjectPicker();
+        setInterval(carouselProjectPicker, 10000);
+    });
+}
